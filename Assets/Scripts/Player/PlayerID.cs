65a8c0017fce486a743c3b2ba6f2e564f2454787
TW-RF54732 <127068503+TW-RF54732@users.noreply.github.com>
2024-04-21 21:12:32 +0800

    +Mesh Generater
    製作網格噪音地圖，存在於GameObject資料夾內

--- Assets/Scripts/Player/PlayerID.cs
+++ Assets/Scripts/Player/PlayerID.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayID : MonoBehaviour
+public class PlayerID : MonoBehaviour
 {
     [SerializeField]
     int ID;
